7fcbc31fd13ff37f8f96bf925c46e361d191dd87
ariel klevecz <ariel@yaytso.art>
2025-04-09 13:49:05 -0700

    font sizing of calc

--- src/lib/components/info/calculator-footer.svelte
+++ src/lib/components/info/calculator-footer.svelte
@@ -3,7 +3,7 @@
     import EstimatedPriceCalculator from './estimated-price-calculator.svelte';
 </script>
 
-<div class="fixed bottom-0 left-0 flex h-10 w-full items-center justify-center bg-[var(--purple)] text-center text-xl">
+<div class="fixed bottom-0 left-0 flex h-10 w-full items-center justify-center bg-[var(--purple)] text-center text-xs md:text-xl p-1">
     <CarbonSavings />
     <EstimatedPriceCalculator/>
 </div>
--- src/lib/components/info/estimated-price-calculator.svelte
+++ src/lib/components/info/estimated-price-calculator.svelte
@@ -2,7 +2,7 @@
     import project from '$lib/project.svelte';
 </script>
 
-<div class="text-center text-xl">
+<div class="text-center p-1">
     <span class="font-semibold">Estimated Cost:</span>
     {project.state.pricing.estimatedCost.toLocaleString('en-US', {
         style: 'currency',
